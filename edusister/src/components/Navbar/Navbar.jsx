188703c32a9816a626a082dc427450b70f8e3e20
Bliss109 <cindy.ogutu@strathmore.edu>
2025-05-31 11:39:55 +0300

    Landing page upto Header

---
+++ edusister/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import '../../main.css'
+
+//Imported icons
+import { GiGlassHeart } from "react-icons/gi";
+import { MdSupportAgent } from "react-icons/md";
+import { MdOutlineLanguage } from "react-icons/md";
+import { CgMenuGridO } from "react-icons/cg";
+
+//Imported images
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='navBar flex'>
+            <div className="navBarOne flex">
+                {/* Left: Heart */}
+                <GiGlassHeart className='icon heartIcon' />
+
+                {/* Center: Support & Language */}
+                <div className="middleLinks flex">
+                    <li className="flex"><MdSupportAgent className='icon' />Support</li>
+                    <li className="flex"><MdOutlineLanguage className='icon' />Language</li>
+                </div>
+
+                {/* Right: Auth Links */}
+                <div className="atb flex">
+                    <span>Sign Up</span>
+                    <span>Log In</span>
+                </div>
+            </div>
+
+            <div className="navBarTwo">
+                <div className="logoDiv">
+                    <img src={logo} alt="EduSister logo" />
+                </div>
+
+                <div className="navBarMenu">
+                    <ul className="menu flex">
+                        <li className="listItem">Home</li>
+                        <li className="listItem">Journal</li>
+                        <li className="listItem">Resources</li>
+                        <li className="listItem">Mentorship</li>
+                        <li className="listItem">SisterCircle</li>
+                    </ul>
+
+                    <button className="btn flex btnOne">
+                        Contact
+                    </button>
+
+                    <div className="toggleIcon">
+                        <CgMenuGridO />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
