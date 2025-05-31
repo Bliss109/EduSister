9db15cb75fc36c75e349d3b5b26700eeec4f6185
Bliss109 <cindy.ogutu@strathmore.edu>
2025-05-31 11:04:24 +0300

    Firebase and React Setup

---
+++ edusister/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV76nlbXAqpHo8GkmFoGxdHCvKDps2ex4",
+  authDomain: "edusister.firebaseapp.com",
+  projectId: "edusister",
+  storageBucket: "edusister.firebasestorage.app",
+  messagingSenderId: "85683269476",
+  appId: "1:85683269476:web:57ce27434648582600d471"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
