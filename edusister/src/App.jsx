9c0f0bf36212cbb919876156ab14ebd6706d7043
Bliss109 <cindy.ogutu@strathmore.edu>
2025-06-08 19:47:39 +0300

    Project Structure modified

--- edusister/src/App.jsx
+++ edusister/src/App.jsx
@@ -1,46 +1,20 @@
-import React from 'react'
+// App.jsx
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/Header/Header'
-import About from './components/About/About'
-import MentorSection from './components/Mentor/MentorSection'
-import ResourcesSection from './components/Resources/ResourceSection'
-import Newsletter from './components/Newsletter/Newsletter'
-import Footer from './components/Footer/Footer'
-import BackToTop from './components/BackToTop/BackToTop'
-import Signup from './Signup';
-import Login from './Login';
-import './global.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <main className='main-content'>
-        <Routes>
-          <Route path="/" element={
-          <>
-            <About />
-            <MentorSection />
-            <ResourcesSection />
-            <Newsletter />
-            <Footer />
-            <BackToTop />
-          </>
-          } />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Header />
-        <About />
-        <MentorSection />
-        <ResourcesSection />
-        <Newsletter />
-        <Footer />
-        <BackToTop />
-      </main>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
