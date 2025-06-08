d78f7513b6075a583737aada1484439b6ed5a38a
Bliss109 <cindy.ogutu@strathmore.edu>
2025-06-08 19:40:48 +0300

    Pages defined and updated

--- edusister/src/App.jsx
+++ edusister/src/App.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/Header/Header'
-import About from './components/About/About'
-import MentorSection from './components/Mentor/MentorSection'
-import ResourcesSection from './components/Resources/ResourceSection'
-import Newsletter from './components/Newsletter/Newsletter'
-import Footer from './components/Footer/Footer'
-import BackToTop from './components/BackToTop/BackToTop'
+// App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <main className='main-content'>
-        <Header />
-        <About />
-        <MentorSection />
-        <ResourcesSection />
-        <Newsletter />
-        <Footer />
-        <BackToTop />
-      </main>
-    </div>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
+
