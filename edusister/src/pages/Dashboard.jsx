f3f78f76709a67507e07c3800830b3cbd68e0f8b
Bliss109 <cindy.ogutu@strathmore.edu>
2025-06-16 19:02:41 +0300

    Student Dashboard complete

--- edusister/src/pages/Dashboard.jsx
+++ edusister/src/pages/Dashboard.jsx
@@ -1,13 +1,20 @@
 // src/pages/Dashboard.jsx
 import React from 'react';
-
+import StudentStats from '../components/student/StudentStats';
+import JournalResume from '../components/student/JournalResume';
+import QuoteAndReflection from '../components/student/QuoteAndReflection';
+import MoodTracker from '../components/student/MoodTracker';
+import ActivePeerChats from '../components/student/ActivePeerChats';
 
 const Dashboard = () => {
   return (
     <div className="main-section">
-       Testing unit
+      <StudentStats />
+      <JournalResume />
+      <QuoteAndReflection />
       <div className="mood-peer-wrapper">
-     
+        <MoodTracker />
+        <ActivePeerChats />
       </div>
     </div>
   );
